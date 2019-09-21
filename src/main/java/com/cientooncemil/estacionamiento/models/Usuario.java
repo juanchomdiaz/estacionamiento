fb30686f1e37a417a038f90132b04263c997d080
aula dig05 <aula dig05@DESKTOP-ENBTG4H.local>
2019-09-21 12:21:15 -0300

    Agregado login de usuario mock

--- src/main/java/com/cientooncemil/estacionamiento/models/Usuario.java
+++ src/main/java/com/cientooncemil/estacionamiento/models/Usuario.java
@@ -2,4 +2,53 @@ package com.cientooncemil.estacionamiento.models;
 
 public class Usuario {
 
+	private String apellido;
+	private String nombre;
+	private String nombreUsuario;
+	private String password;
+
+	public Usuario() {
+		
+	}
+	
+	public Usuario(String apellido, String nombre, String nombreUsuario, String password) {
+		super();
+		this.apellido = apellido;
+		this.nombre = nombre;
+		this.nombreUsuario = nombreUsuario;
+		this.password = password;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
